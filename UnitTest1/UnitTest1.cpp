75816dacdceb868f97dc4a393954c4f395c98bad
User <nazikkocur@gmail,com>
2024-04-11 23:44:47 +0300

    first commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Pr8.1REC/Pr8.1REC.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			char str[] = "abcsqdefg";
+			Assert::AreEqual(1, Count(str, 0));
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			char* dest = new char[151];
+			dest[0] = '\0';
+			char str[] = "sqabcsqdefgqsq";
+
+			char* result = Change(dest, str, dest, 0);
+			Assert::AreEqual("***abc***defg***q", result);
+			delete[] result;
+		}
+	};
+}
